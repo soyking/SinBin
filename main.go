19c242a6b2cde951b00e86e55985f064a55dfddd
soyking <steve.wu@curiositychina.com>
2016-03-13 01:32:04 +0800

    native interface

--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
-	_ "blog/routers"
-	"github.com/astaxie/beego"
+	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
+func logHandler(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now()
+		next.ServeHTTP(w, r)
+		endTime := time.Now()
+		log.Printf("[%s] %s %.3fms", r.Method, r.URL.Path, float64(endTime.Sub(startTime).Nanoseconds())/1000000)
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func staticHandleFunc(w http.ResponseWriter, r *http.Request) {
+	path := strings.Replace(r.URL.Path, "public", "static", 1)[1:]
+	if _, err := os.Stat(path); err == nil {
+		http.ServeFile(w, r, path)
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
 func main() {
-	beego.Run()
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/dist/index.html")
+	})
+
+	http.HandleFunc("/public/dist/", staticHandleFunc)
+	http.Handle("/", alice.New(logHandler).Then(r))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- views/index.tpl
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<head>
-  <meta charset="UTF-8" />
-  <title>Soyking's Page</title>
-</head>
-
-<body>
-  hi, I'm soyking, but not a king.
-</body>
