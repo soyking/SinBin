3501f30521feb0454cb9277329a549bda9448058
soyking <steve.wu@curiositychina.com>
2016-03-27 15:38:16 +0800

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 godep go build -o blog
-
 cd static 
 npm install
 npm run publish 
 rm -rf node_modules
 cd ../
-
 docker stop blog && docker rm blog
 docker rmi ubuntu-blog && docker build -t ubuntu-blog .
 docker run -t --name blog -p 8080:8080 -d ubuntu-blog
